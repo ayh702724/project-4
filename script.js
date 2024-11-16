0e863a9160f491cb94b3d466a328ef278adad490
ayh702724 <ayh702724@gmail.com>
2024-11-16 04:41:23 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("btn1").onclick =function(){
+    alert(" تم نسخ النص")  }
+    document.getElementById("btn2").onclick =function(){
+        alert(" تم نسخ النص")  }
+        document.getElementById("btn3").onclick =function(){
+            alert(" تم نسخ النص")  }
